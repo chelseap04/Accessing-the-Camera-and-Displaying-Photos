a3b46a8c352a5110f0cf359249951407ca2a5b48
Chelsea Poppleton <chelseap@new.rr.com>
2023-05-27 10:20:35 -0500

    Initial Commit

---
+++ Accessing the Camera and Displaying Photos/Accessing_the_Camera_and_Displaying_PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessing_the_Camera_and_Displaying_PhotosApp.swift
+//  Accessing the Camera and Displaying Photos
+//
+//  Created by Chelsea Poppleton on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Accessing_the_Camera_and_Displaying_PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
